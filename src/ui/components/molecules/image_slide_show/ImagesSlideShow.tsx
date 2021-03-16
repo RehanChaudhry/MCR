840d525ae9223046305940558c55c31ced3a78de
Jaweria Siddiqui <jaweria.siddiqui@startrum.com>
2021-03-16 17:20:43 +0500

    worked on image slider

--- src/ui/components/molecules/image_slide_show/ImagesSlideShow.tsx
+++ src/ui/components/molecules/image_slide_show/ImagesSlideShow.tsx
@@ -1,6 +1,6 @@
 import ArrowLeft from "assets/images/left.svg";
 import ArrowRight from "assets/images/right.svg";
-import React from "react";
+import React, { useRef, useState } from "react";
 import { StyleSheet, TouchableOpacityProps, View } from "react-native";
 import { SliderBox } from "react-native-image-slider-box";
 import { Color, NumberProp } from "react-native-svg";
@@ -8,7 +8,7 @@ import {
   AppImageBackground,
   CONTAINER_TYPES
 } from "ui/components/atoms/image_background/AppImageBackground";
-import { SvgProp } from "utils/Util";
+import { AppLog, SvgProp } from "utils/Util";
 
 export interface ImageSlideShowProps extends TouchableOpacityProps {
   images: string[];
@@ -43,43 +43,76 @@ const rightIcon: SvgProp = (
   );
 };
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const leftImage = () => {
-  return (
-    <AppImageBackground
-      containerShape={CONTAINER_TYPES.CIRCLE}
-      icon={leftIcon}
-    />
-  );
-};
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const rightImage = () => {
-  return (
-    <AppImageBackground
-      containerShape={CONTAINER_TYPES.CIRCLE}
-      icon={rightIcon}
-    />
-  );
-};
-
 export const ImagesSlideShow = React.memo<ImageSlideShowProps>(
   ({ images }) => {
+    const [selectedIndex, setSelectedIndex] = useState<number>(0);
+    const arrowButton = useRef<SliderBox | null>(null);
+    const onLeftImagePress = () => {
+      AppLog.logForcefully(selectedIndex);
+      if (selectedIndex > 0) {
+        arrowButton.current.onSnap(selectedIndex - 1);
+      }
+    };
+    const onRightImagePress = () => {
+      AppLog.logForcefully(selectedIndex);
+      arrowButton.current.onSnap(selectedIndex);
+    };
+
+    const leftImage = () => {
+      return (
+        <AppImageBackground
+          containerShape={CONTAINER_TYPES.CIRCLE}
+          icon={leftIcon}
+          onPress={onLeftImagePress}
+        />
+      );
+    };
+
+    const rightImage = () => {
+      return (
+        <AppImageBackground
+          containerShape={CONTAINER_TYPES.CIRCLE}
+          icon={rightIcon}
+          onPress={onRightImagePress}
+        />
+      );
+    };
     return (
       <View style={styles.MainContainer}>
-        <SliderBox
-          images={images}
-          dotColor="#FFEE58"
-          inactiveDotColor="#90A4AE"
-          paginationBoxStyle={styles.paginationColor}
-          paginationBoxVerticalPadding={3}
-          dotStyle={{
-            width: 10,
-            height: 10,
-            borderRadius: 5
-          }}
-          ImageComponentStyle={styles.image}
-        />
+        <View style={{ overflow: "visible" }}>
+          <SliderBox
+            ref={arrowButton}
+            images={images}
+            dotColor="#FFEE58"
+            inactiveDotColor="#90A4AE"
+            paginationBoxStyle={styles.paginationColor}
+            paginationBoxVerticalPadding={3}
+            dotStyle={{
+              width: 10,
+              height: 10,
+              borderRadius: 5
+            }}
+            ImageComponentStyle={styles.image}
+            sliderBoxHeight={350}
+            onCurrentImagePressed={(index: number) =>
+              setSelectedIndex(index)
+            }
+            currentImageEmitter={(index: number) =>
+              setSelectedIndex(index)
+            }
+          />
+        </View>
+        <View
+          style={{
+            position: "absolute",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            width: "90%",
+            top: "45%"
+          }}>
+          {leftImage()}
+          {rightImage()}
+        </View>
         {/*<Slideshow*/}
         {/*  dataSource={images}*/}
         {/*  indicatorColor="gray"*/}
