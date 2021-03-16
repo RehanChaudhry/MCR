361c80ff53fdcea3e02f4785e1228882a0c2fe5b
Abdul Rehman <anis.ghazi@startrum.com>
2021-03-16 18:15:18 +0500

    added header in questionnaire screen

--- src/ui/screens/questions/QuestionsView.tsx
+++ src/ui/screens/questions/QuestionsView.tsx
@@ -5,9 +5,13 @@ import SectionedList, {
 } from "ui/components/organisms/sectioned_list/SectionedList";
 import QuestionHeader from "ui/components/molecules/question_header/QuestionHeader";
 import QuestionSection from "models/QuestionSection";
-import { SPACE } from "config";
+import { FONT_SIZE, SPACE, STRINGS } from "config";
 import Question from "models/Question";
 import { QuestionItem } from "ui/components/organisms/question_item/QuestionItem";
+import { AppLabel } from "ui/components/atoms/app_label/AppLabel";
+import { HeadingWithText } from "ui/components/molecules/heading_with_text/HeadingWithText";
+import { usePreferredTheme } from "hooks";
+import { AppButton } from "ui/components/molecules/app_button/AppButton";
 
 type Props = {
   submitAnswersLoading: boolean;
@@ -16,9 +20,38 @@ type Props = {
 };
 
 export const QuestionsView = ({ questions }: Props) => {
+  const { themedColors } = usePreferredTheme();
+
+  const listHeader = () => (
+    <View style={styles.headerContainer}>
+      <AppLabel
+        style={styles.infoText}
+        text={STRINGS.questionnaire.info}
+        numberOfLines={0}
+      />
+      <View
+        style={[
+          styles.headerCard,
+          {
+            backgroundColor: themedColors.background,
+            borderColor: themedColors.border
+          }
+        ]}>
+        <HeadingWithText
+          headingText={STRINGS.questionnaire.how_it_works}
+          headingStyle={styles.infoCardHeading}
+          text={STRINGS.questionnaire.how_it_works_detail}
+          textStyle={styles.infoCardText}
+        />
+        <AppButton text={STRINGS.questionnaire.learn_more} />
+      </View>
+    </View>
+  );
+
   return (
-    <View>
+    <View style={styles.container}>
       <SectionedList<QuestionSection, Question>
+        listHeaderComponent={listHeader()}
         style={styles.sectionedList}
         list={questions}
         headerView={(
@@ -59,5 +92,35 @@ const styles = StyleSheet.create({
     borderBottomStartRadius: 5,
     borderBottomEndRadius: 5,
     marginBottom: SPACE.sm
+  },
+  container: {
+    flexDirection: "column"
+  },
+  infoText: {
+    fontSize: FONT_SIZE.sm,
+    marginBottom: SPACE.md,
+    textAlign: "center"
+  },
+  headerContainer: {
+    flexDirection: "column"
+  },
+  headerCard: {
+    overflow: "hidden",
+    borderRadius: 5,
+    marginVertical: SPACE.sm,
+    padding: SPACE.md,
+
+    // shadow
+    shadowColor: "#000000",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+    elevation: 2
+  },
+  infoCardHeading: {
+    fontSize: FONT_SIZE.md
+  },
+  infoCardText: {
+    fontSize: FONT_SIZE.sm
   }
 });
