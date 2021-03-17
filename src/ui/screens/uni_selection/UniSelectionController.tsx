d03e3c7e28d74cd7d51c502728ed72c4bf949a13
Muhammad Zeeshan <mzeeshanid@yahoo.com>
2021-03-17 11:57:30 +0500

    - uni selection initial design implementation

---
+++ src/ui/screens/uni_selection/UniSelectionController.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react";
+import UniSelectionView from "./UniSelectionView";
+
+type Props = {};
+
+const UniSelectionController: FC<Props> = () => {
+  return <UniSelectionView />;
+};
+
+export default UniSelectionController;
