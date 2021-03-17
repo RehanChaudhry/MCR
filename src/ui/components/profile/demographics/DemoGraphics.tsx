c08ca7037cc8a09cc0bc8b0e67f47aba40e6c9ab
RehanChaudhry <m.rehan@startrum.com>
2021-03-17 15:48:05 +0500

    demographics work

--- src/ui/components/profile/demographics/DemoGraphics.tsx
+++ src/ui/components/profile/demographics/DemoGraphics.tsx
@@ -5,33 +5,117 @@ import { StyleSheet, View } from "react-native";
 import usePreferredTheme from "hooks/theme/usePreferredTheme";
 import { SPACE } from "config";
 import { AppFormDropDown } from "../../molecules/app_form_dropdown/AppFormDropDown";
+import { AppFormRadioButton } from "../../molecules/app_form_radio_buttons/AppFormRadioButton";
+import { DIRECTION_TYPE } from "../../atoms/radio_group/RadioGroup";
+import AppFormField from "../../molecules/app_form/AppFormField";
+import * as Yup from "yup";
+import { FormikValues } from "formik";
+import AppForm from "../../molecules/app_form/AppForm";
 
-export const DemoGraphics = React.memo(() => {
-  const theme = usePreferredTheme();
-  return (
-    <CardView style={styles.cardStyles}>
-      <HeadingWithText
-        headingText={"Demographics"}
-        text={"Please take a moment to tell us more about you."}
-        headingStyle={[
-          styles.headingStyle,
-          { color: theme.themedColors.label }
-        ]}
-        textStyle={[{ color: theme.themedColors.labelSecondary }]}
-      />
-      <View
-        style={[
-          styles.horizontalLine,
-          { backgroundColor: theme.themedColors.interface["700"] }
-        ]}
-      />
-
-      <AppFormDropDown
-        labelProps={{ text: "Gender", weight: "semi-bold" }}
-      />
-    </CardView>
-  );
+//forms validation
+const validationSchema = Yup.object().shape({
+  homeTown: Yup.string().required("Enter your hometown"),
+  intendedMajor: Yup.string().required("Enter your intended major")
 });
+
+export const DemoGraphics = React.memo(
+  ({
+    // @ts-ignore
+    initialValues = {
+      homeTown: "",
+      intendedMajor: ""
+    },
+    // @ts-ignore
+    onSubmit = (_values: FormikValues) => {}
+  }) => {
+    const theme = usePreferredTheme();
+    return (
+      <CardView style={styles.cardStyles}>
+        <HeadingWithText
+          headingText={"Demographics"}
+          text={"Please take a moment to tell us more about you."}
+          headingStyle={[
+            styles.headingStyle,
+            { color: theme.themedColors.label }
+          ]}
+          textStyle={[{ color: theme.themedColors.labelSecondary }]}
+        />
+        <View
+          style={[
+            styles.horizontalLine,
+            { backgroundColor: theme.themedColors.interface["700"] }
+          ]}
+        />
+
+        <AppFormDropDown
+          labelProps={{ text: "Gender", weight: "semi-bold" }}
+          viewStyle={[
+            styles.viewFieldStyle,
+            { borderColor: theme.themedColors.border }
+          ]}
+        />
+        <View style={styles.spacer} />
+
+        <AppFormRadioButton
+          labelProps={{ text: "Smoking Habits", weight: "semi-bold" }}
+          radioData={[
+            { id: 1, label: "never" },
+            { id: 2, label: "Occasionally" },
+            { id: 3, label: "Frequently" }
+          ]}
+          direction={DIRECTION_TYPE.HORIZONTAL}
+        />
+        <View style={styles.spacer} />
+        <AppForm
+          initialValues={initialValues}
+          onSubmit={onSubmit}
+          validationSchema={validationSchema}>
+          <AppFormField
+            fieldTestID="homeTown"
+            validationLabelTestID={"homeTownValidationLabel"}
+            name="homeTown"
+            labelProps={{ text: "Hometown", weight: "semi-bold" }}
+            fieldInputProps={{
+              textContentType: "name",
+              keyboardType: "default",
+              returnKeyType: "next",
+              placeholder: "Enter your hometown",
+              autoCapitalize: "none",
+              viewStyle: [
+                styles.textFieldStyle,
+                {
+                  backgroundColor: theme.themedColors.background,
+                  borderColor: theme.themedColors.border
+                }
+              ]
+            }}
+          />
+          <View style={styles.spacer} />
+          <AppFormField
+            fieldTestID="intendedMajor"
+            validationLabelTestID={"intendedMajorValidationLabel"}
+            name="intendedMajor"
+            labelProps={{ text: "Intended Major", weight: "semi-bold" }}
+            fieldInputProps={{
+              textContentType: "name",
+              keyboardType: "default",
+              returnKeyType: "next",
+              placeholder: "Enter your intended major",
+              autoCapitalize: "none",
+              viewStyle: [
+                styles.textFieldStyle,
+                {
+                  backgroundColor: theme.themedColors.background,
+                  borderColor: theme.themedColors.border
+                }
+              ]
+            }}
+          />
+        </AppForm>
+      </CardView>
+    );
+  }
+);
 
 const styles = StyleSheet.create({
   cardStyles: {
@@ -45,5 +129,14 @@ const styles = StyleSheet.create({
   horizontalLine: {
     height: 0.5,
     marginVertical: SPACE.lg
+  },
+  viewFieldStyle: {
+    borderWidth: 1
+  },
+  textFieldStyle: {
+    borderWidth: 1
+  },
+  spacer: {
+    paddingBottom: SPACE.lg
   }
 });
