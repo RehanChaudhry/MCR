de2f1254f0b18d0230022a508ec3334cd09faf87
Muhammad Muzammil <muhammadmuzammilqadri@live.com>
2021-03-25 18:02:33 +0500

    Debugging Performance

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,70 @@
 // prettier-ignore
 import "react-native-gesture-handler";
 import { AppColorScheme, AppThemeProvider } from "hooks/theme";
-import React from "react";
-import { SplashView } from "ui/screens/auth/splash/SplashView";
+import React, { FC, useState } from "react";
+import { Button, StyleProp, Text, TextStyle, View } from "react-native";
+import {
+  SafeAreaProvider,
+  SafeAreaView
+} from "react-native-safe-area-context";
 import { AppLog } from "utils/Util";
-import { SafeAreaProvider } from "react-native-safe-area-context";
 
 type Props = {};
 
+type LabelProps = {
+  text: string;
+  style?: StyleProp<TextStyle>;
+};
+const MyLabel = React.memo<LabelProps>(
+  (props) => {
+    AppLog.log("Rendering MyLabel...");
+    return <Text style={props.style}>{props.text}</Text>;
+  },
+  (prevProps, nextProps) => {
+    return true;
+  }
+);
+
+type ButtonProps = {
+  text: string;
+  onPress: () => void;
+};
+const MyButton: FC<ButtonProps> = (props) => {
+  AppLog.log("Rendering MyButton...");
+  return <Button title={props.text} onPress={props.onPress} />;
+};
+
 const App: React.FC<Props> = () => {
   AppLog.log("Rendering App...");
-
+  const [counter, setCounter] = useState(0);
+  // const theme = usePreferredTheme();
   return (
     <AppThemeProvider colorScheme={AppColorScheme.SYSTEM}>
       <SafeAreaProvider>
-        <SplashView />
+        <SafeAreaView
+          style={[
+            {
+              flex: 1,
+              width: "100%",
+              alignItems: "center"
+            }
+          ]}>
+          <View>
+            <MyLabel
+              text="My Label"
+              style={[{ backgroundColor: "#FFF456" }]}
+            />
+            <MyButton
+              text="Press Me"
+              onPress={() => {
+                setCounter(counter + 1);
+              }}
+            />
+          </View>
+        </SafeAreaView>
       </SafeAreaProvider>
     </AppThemeProvider>
   );
 };
 export default App;
+
