a6cf1e8ad638d4fa0527f1636aa2a5085bce805d
Muhammad Noman <m.nomaniu@gmail.com>
2021-03-26 12:53:41 +0500

    add toolbar in welcome view screen

--- src/ui/screens/home/welcome/WelcomeController.tsx
+++ src/ui/screens/home/welcome/WelcomeController.tsx
@@ -1,8 +1,12 @@
 import { StackNavigationProp } from "@react-navigation/stack";
 import { AuthStackParamList } from "routes";
-import React, { FC, useLayoutEffect } from "react";
+import React, { FC } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { WelcomeView } from "ui/screens/home/welcome/WelcomeView";
+import HeaderRightTextWithIcon from "ui/components/molecules/header_right_text_with_icon/HeaderRightTextWithIcon";
+import { HeaderTitle } from "ui/components/molecules/header_title/HeaderTitle";
+import { usePreferredTheme } from "hooks";
+import RightArrow from "assets/images/right.svg";
 
 type LoginNavigationProp = StackNavigationProp<
   AuthStackParamList,
@@ -13,13 +17,27 @@ type Props = {};
 
 const WelcomeController: FC<Props> = () => {
   const navigation = useNavigation<LoginNavigationProp>();
+  const theme = usePreferredTheme();
 
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      headerTitleAlign: "center",
-      title: "Notification"
-    });
-  }, [navigation]);
+  navigation.setOptions({
+    headerRight: () => (
+      <HeaderRightTextWithIcon
+        text="Skip"
+        textStyle={{ color: theme.themedColors.interface["700"] }}
+        icon={() => {
+          return (
+            <RightArrow
+              width={20}
+              height={20}
+              fill={theme.themedColors.interface["700"]}
+            />
+          );
+        }}
+      />
+    ),
+    headerTitleAlign: "center",
+    headerTitle: () => <HeaderTitle text="Watch Video" />
+  });
 
   return <WelcomeView />;
 };
