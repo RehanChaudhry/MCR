147278ea5c0988d19d2639632cc5d0d02f640961
Jaweria Siddiqui <jaweria.siddiqui@startrum.com>
2021-04-07 14:25:36 +0500

    resolved keyboard issue in report content screen

--- src/ui/screens/home/community/report_content/ReportContentView.tsx
+++ src/ui/screens/home/community/report_content/ReportContentView.tsx
@@ -2,7 +2,7 @@ import { FONT_SIZE, SPACE, STRINGS } from "config";
 import Strings from "config/Strings";
 import { usePreferredTheme } from "hooks";
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 import { AppLabel } from "ui/components/atoms/app_label/AppLabel";
 import CheckboxWithText from "ui/components/atoms/CheckboxWithText";
 import Screen from "ui/components/atoms/Screen";
@@ -19,99 +19,101 @@ export const ReportContentView = React.memo<Props>(({ closeScreen }) => {
 
   return (
     <Screen style={styles.mainContainer}>
-      <View style={styles.container}>
-        <AppLabel
-          text={Strings.reportContent.selectProblemToCont}
-          weight="semi-bold"
-          style={styles.heading}
-        />
-        <AppLabel
-          text={Strings.reportContent.reportPostAfterSelectingProb}
-          style={[
-            styles.marginTopXS,
-            { color: theme.themedColors.interface["700"] },
-            styles.subHeading
-          ]}
-          numberOfLines={0}
-        />
-        <CheckboxWithText
-          text={Strings.reportContent.offensiveContent}
-          onChange={(value) => AppLog.log(value)}
-          style={styles.marginTopLg}
-        />
-        <CheckboxWithText
-          text={Strings.reportContent.harassment}
-          onChange={(value) => AppLog.log(value)}
-        />
-        <CheckboxWithText
-          text={Strings.reportContent.bullying}
-          onChange={(value) => AppLog.log(value)}
-        />
-        <CheckboxWithText
-          text={Strings.reportContent.spam}
-          onChange={(value) => AppLog.log(value)}
-        />
-        <CheckboxWithText
-          text={Strings.reportContent.violence}
-          onChange={(value) => AppLog.log(value)}
-        />
-        <CheckboxWithText
-          text={Strings.reportContent.concerningContent}
-          onChange={(value) => AppLog.log(value)}
-        />
-        <CheckboxWithText
-          text={Strings.reportContent.nudity}
-          onChange={(value) => AppLog.log(value)}
-        />
-        <CheckboxWithText
-          text={Strings.reportContent.threats}
-          onChange={(value) => AppLog.log(value)}
-        />
-        <CheckboxWithText
-          text={Strings.reportContent.hateSpeech}
-          onChange={(value) => AppLog.log(value)}
-        />
-        <CheckboxWithText
-          text={Strings.reportContent.somethingElse}
-          onChange={(value) => AppLog.log(value)}
-        />
-        <AppLabel
-          text={Strings.reportContent.reason}
-          weight="semi-bold"
-          style={[styles.marginTopLg, styles.subHeading]}
-        />
-        <View style={styles.reasonContainer}>
-          <AppInputField
-            textAlignVertical={"top"}
-            viewStyle={[
-              styles.descriptionView,
-              {
-                backgroundColor: theme.themedColors.background,
-                borderColor: theme.themedColors.secondary
-              }
-            ]}
-            multiline={true}
-            numberOfLines={6}
-            placeholderTextColor={theme.themedColors.interface["600"]}
-            placeholder={STRINGS.reportContent.reasonPlaceholder}
+      <ScrollView keyboardShouldPersistTaps={"handled"}>
+        <View style={styles.container}>
+          <AppLabel
+            text={Strings.reportContent.selectProblemToCont}
+            weight="semi-bold"
+            style={styles.heading}
+          />
+          <AppLabel
+            text={Strings.reportContent.reportPostAfterSelectingProb}
             style={[
-              { color: theme.themedColors.label },
-              styles.inputFieldRow
+              styles.marginTopXS,
+              { color: theme.themedColors.interface["700"] },
+              styles.subHeading
             ]}
+            numberOfLines={0}
+          />
+          <CheckboxWithText
+            text={Strings.reportContent.offensiveContent}
+            onChange={(value) => AppLog.log(value)}
+            style={styles.marginTopLg}
+          />
+          <CheckboxWithText
+            text={Strings.reportContent.harassment}
+            onChange={(value) => AppLog.log(value)}
+          />
+          <CheckboxWithText
+            text={Strings.reportContent.bullying}
+            onChange={(value) => AppLog.log(value)}
+          />
+          <CheckboxWithText
+            text={Strings.reportContent.spam}
+            onChange={(value) => AppLog.log(value)}
+          />
+          <CheckboxWithText
+            text={Strings.reportContent.violence}
+            onChange={(value) => AppLog.log(value)}
+          />
+          <CheckboxWithText
+            text={Strings.reportContent.concerningContent}
+            onChange={(value) => AppLog.log(value)}
+          />
+          <CheckboxWithText
+            text={Strings.reportContent.nudity}
+            onChange={(value) => AppLog.log(value)}
+          />
+          <CheckboxWithText
+            text={Strings.reportContent.threats}
+            onChange={(value) => AppLog.log(value)}
+          />
+          <CheckboxWithText
+            text={Strings.reportContent.hateSpeech}
+            onChange={(value) => AppLog.log(value)}
+          />
+          <CheckboxWithText
+            text={Strings.reportContent.somethingElse}
+            onChange={(value) => AppLog.log(value)}
+          />
+          <AppLabel
+            text={Strings.reportContent.reason}
+            weight="semi-bold"
+            style={[styles.marginTopLg, styles.subHeading]}
+          />
+          <View style={styles.reasonContainer}>
+            <AppInputField
+              textAlignVertical={"top"}
+              viewStyle={[
+                styles.descriptionView,
+                {
+                  backgroundColor: theme.themedColors.background,
+                  borderColor: theme.themedColors.secondary
+                }
+              ]}
+              multiline={true}
+              numberOfLines={6}
+              placeholderTextColor={theme.themedColors.interface["600"]}
+              placeholder={STRINGS.reportContent.reasonPlaceholder}
+              style={[
+                { color: theme.themedColors.label },
+                styles.inputFieldRow
+              ]}
+            />
+          </View>
+
+          <AppButton
+            text={Strings.reportContent.submitSpamReport}
+            buttonStyle={[
+              { backgroundColor: theme.themedColors.danger },
+              styles.marginTopLg
+            ]}
+            textStyle={{ color: theme.themedColors.background }}
+            fontWeight="bold"
+            onPress={closeScreen}
           />
         </View>
-
-        <AppButton
-          text={Strings.reportContent.submitSpamReport}
-          buttonStyle={[
-            { backgroundColor: theme.themedColors.danger },
-            styles.marginTopLg
-          ]}
-          textStyle={{ color: theme.themedColors.background }}
-          fontWeight="bold"
-          onPress={closeScreen}
-        />
-      </View>
+      </ScrollView>
     </Screen>
   );
 });
@@ -128,7 +130,7 @@ const styles = StyleSheet.create({
   },
   inputFieldRow: {
     flex: 1,
-    paddingTop: SPACE._2xs
+    marginTop: SPACE.xs
   },
   descriptionView: {
     height: 100,
