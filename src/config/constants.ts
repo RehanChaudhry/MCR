2fa9962456cdc0d6a67cd55d613f58b9d59a35b0
Asad Ali <asad.ali@cygnismedia.com>
2021-06-01 15:22:56 +0500

    create hook to handle image upload some refactoring

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,7 +2,5 @@ import Env from "envs/env";
 
 export default {
   SHOULD_PRINT_LOGS: true,
-  SHOULD_ENABLE_FORCE_UPDATE: Env.SHOULD_ENABLE_FORCE_UPDATE,
-  S3_BUCKET_URL:
-    "https://my-college-roomie-dev.s3.us-west-2.amazonaws.com/"
+  SHOULD_ENABLE_FORCE_UPDATE: Env.SHOULD_ENABLE_FORCE_UPDATE
 };
--- src/ui/screens/home/community/create_post/CreatePostController.tsx
+++ src/ui/screens/home/community/create_post/CreatePostController.tsx
@@ -19,14 +19,7 @@ import { CommunityStackParamList } from "routes/CommunityStack";
 import HeaderLeftTextWithIcon from "ui/components/molecules/header_left_text_with_icon/HeaderLeftTextWithIcon";
 import { HeaderTitle } from "ui/components/molecules/header_title/HeaderTitle";
 import { CreatePostView } from "ui/screens/home/community/create_post/CreatePostView";
-import { AppLog } from "utils/Util";
-import { ImagePickerResponse } from "react-native-image-picker";
-import OtherApis from "repo/home/OtherApis";
-import { GenerateSignedUrlResponse } from "models/api_responses/GenerateSignedUrlResponse";
-import SimpleToast from "react-native-simple-toast";
-import _ from "lodash";
-import { S3ImageUploadRequest } from "models/api_requests/S3ImageUploadRequest";
-import { Constants } from "config";
+import MyImagePickerResponse from "models/api_responses/MyImagePickerResponse";
 
 type CommunityNavigationProp = StackNavigationProp<
   CommunityStackParamList,
@@ -41,7 +34,6 @@ const CreatePostController: FC<Props> = () => {
   const navigation = useNavigation<CommunityNavigationProp>();
   const theme = usePreferredTheme();
   const [showProgressBar, setShowProgressBar] = useState<boolean>(false);
-  const signedUrls = useRef<string[]>([]);
   const route = useRoute<CommunityRoutes>();
 
   useLayoutEffect(() => {
@@ -71,14 +63,6 @@ const CreatePostController: FC<Props> = () => {
     CreatePostApiResponseModel
   >(CommunityAnnouncementApis.createPost);
 
-  const createSignedUrl = useApi<string, GenerateSignedUrlResponse>(
-    OtherApis.createSignedUrl
-  );
-
-  const uploadFileToS3 = useApi<S3ImageUploadRequest, any>(
-    OtherApis.uploadFileToS3
-  );
-
   const handleCreatePost = usePreventDoubleTap(async () => {
     if (requestModel.current === undefined) {
       return;
@@ -99,50 +83,17 @@ const CreatePostController: FC<Props> = () => {
     }
   });
 
-  const createSignedUrlApi = async (image: ImagePickerResponse) => {
-    const { hasError, dataBody } = await createSignedUrl.request([
-      image.fileName!!
-    ]);
-
-    if (hasError || dataBody === undefined) {
-      SimpleToast.show("file upload failed: " + image.fileName!!);
-      return;
-    } else {
-      AppLog.log("Your Signed Url : " + JSON.stringify(dataBody));
-      signedUrls.current.push(image.fileName!!);
-      await handleUploadFileToS3({ url: dataBody.url, data: image });
-    }
-  };
-
-  const handleUploadFileToS3 = async (request: S3ImageUploadRequest) => {
-    AppLog.log("in handleUploadFileToS3()..." + JSON.stringify(request));
-    const {
-      hasError,
-      dataBody,
-      errorBody
-    } = await uploadFileToS3.request([request]);
-
-    if (hasError || dataBody === undefined) {
-      SimpleToast.show("S3 image upload failed" + errorBody);
-      return;
-    } else {
-      AppLog.logForcefully(
-        "Image successfully uploaded to s3 : " + JSON.stringify(dataBody)
-      );
-    }
-  };
-
   const onSubmit = (values: FormikValues) => {
     requestModel.current.content = values.message;
     requestModel.current.link =
       values.link !== "" ? values.link : undefined;
     requestModel.current.embed =
       values.embed !== "" ? values.embed : undefined;
-    requestModel.current.photos = signedUrls.current.reduce(
-      (newImage: Photo[], image: string) => (
+    requestModel.current.photos = values.images.reduce(
+      (newImage: Photo[], image: MyImagePickerResponse) => (
         newImage.push({
-          fileURL: Constants.S3_BUCKET_URL + image,
-          originalName: image
+          fileURL: image.s3Url,
+          originalName: image.fileName
         } as Photo),
         newImage
       ),
@@ -157,25 +108,10 @@ const CreatePostController: FC<Props> = () => {
     route.params?.postCreatedSuccessfully?.();
   });
 
-  const removeSignedImageUrl = (filename: string, removeAll: boolean) => {
-    removeAll
-      ? (signedUrls.current = [])
-      : (signedUrls.current = _.reject(signedUrls.current, (item) =>
-          item.includes(filename)
-        ));
-
-    AppLog.log(
-      "signed urls after removing file name : " +
-        JSON.stringify(signedUrls.current)
-    );
-  };
-
   return (
     <CreatePostView
       createPost={onSubmit}
       shouldShowProgressBar={showProgressBar}
-      createSignedUrl={createSignedUrlApi}
-      removeSignedImageUrl={removeSignedImageUrl}
     />
   );
 };
