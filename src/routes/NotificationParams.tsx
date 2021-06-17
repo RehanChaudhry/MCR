cc71474d74b8305cc5a7e5ce50fbbc390025f818
Muhammad Noman <muhammad.nauman@startrum.com>
2021-06-17 17:36:48 +0500

    set messages in notification

--- src/routes/NotificationParams.tsx
+++ src/routes/NotificationParams.tsx
@@ -1,9 +1,12 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import EScreen from "models/enums/EScreen";
+import { ConnectRequestType } from "ui/screens/home/friends/connect_requests/ConnectRequestsController";
 
 export type NotificationParamList = {
   Notification: undefined;
   ViewProfile: { isFrom: EScreen; updateProfile: boolean };
+  FriendRequests: { title: string; type: ConnectRequestType };
+  Chat: undefined;
 };
 
 export const NotificationStack = createStackNavigator<NotificationParamList>();
