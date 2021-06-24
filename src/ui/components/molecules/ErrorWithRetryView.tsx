de7aa47ffeb9cf5fa5e9623d2d0908c257230f46
Abdul Rehman <anis.ghazi@startrum.com>
2021-06-24 12:07:59 +0500

    Reverted error screen changes

--- src/ui/components/molecules/ErrorWithRetryView.tsx
+++ src/ui/components/molecules/ErrorWithRetryView.tsx
@@ -8,10 +8,8 @@ import {
 } from "ui/components/molecules/app_button/AppButton";
 import { AppLabel } from "ui/components/atoms/app_label/AppLabel";
 import { optimizedMemo } from "ui/components/templates/optimized_memo/optimized_memo";
-import { SvgProp } from "utils/Util";
 
 type Props = {
-  image?: SvgProp;
   text?: string;
   retryCallback?: () => void;
   style?: StyleProp<ViewStyle>;
@@ -20,7 +18,6 @@ type Props = {
 
 const ErrorWithRetryView = optimizedMemo<Props>(
   ({
-    image,
     text = STRINGS.common.some_thing_bad_happened,
     retryCallback = () => {},
     style,
@@ -29,11 +26,6 @@ const ErrorWithRetryView = optimizedMemo<Props>(
     const theme = usePreferredTheme();
     return (
       <View testID="error" style={[style, styles.container]}>
-        {image && (
-          <View style={styles.imageContainer}>
-            {image?.(theme.themedColors.primary, 100, 100)}
-          </View>
-        )}
         <AppLabel
           text={text}
           weight={"semi-bold"}
@@ -41,13 +33,9 @@ const ErrorWithRetryView = optimizedMemo<Props>(
           {...rest}
         />
         <AppButton
-          buttonStyle={[
-            {
-              backgroundColor: theme.themedColors.primary
-            },
-            styles.buttonStyles
-          ]}
-          textContainerStyle={styles.buttonTextContainer}
+          buttonStyle={{
+            backgroundColor: theme.themedColors.primary
+          }}
           textStyle={{ color: theme.themedColors.background }}
           fontWeight={"semi-bold"}
           text={STRINGS.common.try_again}
@@ -62,21 +50,15 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: "column",
     justifyContent: "center",
-    height: "100%",
     alignItems: "center",
     padding: SPACE.lg
   },
   text: {
     fontSize: FONT_SIZE.base,
+    width: "100%",
     textAlign: "center",
     margin: 20
-  },
-  buttonStyles: {
-    width: undefined,
-    paddingHorizontal: SPACE.xl
-  },
-  buttonTextContainer: { flex: undefined },
-  imageContainer: { marginBottom: SPACE.xl }
+  }
 });
 
 export default ErrorWithRetryView;
