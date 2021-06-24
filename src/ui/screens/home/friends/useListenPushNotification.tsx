1a5416587baeadea0574d3bb03c829cd3c86759f
Muhammad Muzammil <muhammad.muzammil@startrum.com>
2021-06-24 14:55:48 +0500

    implementing hook for push notifications

--- src/ui/screens/home/friends/useListenPushNotification.tsx
+++ src/ui/screens/home/friends/useListenPushNotification.tsx
@@ -1,37 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 
-type PushNotificationContext = {
+export type PushNotificationContext = {
   screenName: string;
   notificationId: string;
   setNotification?: (screenName: string, notificationId: string) => void;
 };
 
-const PushNotificationContext = React.createContext<PushNotificationContext>(
+export const PushNotificationContext = React.createContext<PushNotificationContext>(
   {
     screenName: "",
     notificationId: ""
   }
 );
-
-export const AppPushNotificationsProvider = (props: any) => {
-  const [notificationId, setNotificationId] = useState("");
-  const [screenName, setScreenName] = useState("");
-
-  const data: PushNotificationContext = {
-    notificationId: notificationId,
-    screenName: screenName,
-    setNotification: (_notificationId, _screenName) => {
-      setNotificationId(_notificationId);
-      setScreenName(_screenName);
-    }
-  };
-
-  return (
-    <PushNotificationContext.Provider value={data}>
-      {props.children}
-    </PushNotificationContext.Provider>
-  );
-};
 
 const useListenPushNotifications = () =>
   React.useContext(PushNotificationContext);
