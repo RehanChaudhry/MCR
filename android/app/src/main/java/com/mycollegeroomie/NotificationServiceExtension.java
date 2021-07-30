3d598fee64150a4c4eded1a644651c29ab7a9747
Asad Ali <asad.ali@cygnismedia.com>
2021-07-30 17:28:20 +0500

    handle android badge count increment natively for android

--- android/app/src/main/java/com/mycollegeroomie/NotificationServiceExtension.java
+++ android/app/src/main/java/com/mycollegeroomie/NotificationServiceExtension.java
@@ -1,25 +1,28 @@
 package com.mycollegeroomie;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
-
 import org.json.JSONObject;
-
 import com.onesignal.OSNotification;
-import com.onesignal.OSMutableNotification;
 import com.onesignal.OSNotificationReceivedEvent;
 import com.onesignal.OneSignal.OSRemoteNotificationReceivedHandler;
+import com.onesignal.shortcutbadger.ShortcutBadger;
 
 @SuppressWarnings("unused")
 public class NotificationServiceExtension implements OSRemoteNotificationReceivedHandler {
+
+    private SharedPreferences mPrefs;
 
     @Override
     public void remoteNotificationReceived(Context context, OSNotificationReceivedEvent notificationReceivedEvent) {
         OSNotification notification = notificationReceivedEvent.getNotification();
 
-        // Example of modifying the notification's accent color
-        OSMutableNotification mutableNotification = notification.mutableCopy();
-        mutableNotification.setExtender(builder -> builder.setColor(context.getResources().getColor(R.color.colorPrimary)));
+        mPrefs = context.getSharedPreferences("BadgeCountFile", Context.MODE_PRIVATE);
+
+        //Example of modifying the notification's accent color
+//        OSMutableNotification mutableNotification = notification.mutableCopy();
+//        mutableNotification.setExtender(builder -> builder.setColor(context.getResources().getColor(R.color.colorPrimary)));
 
         JSONObject data = notification.getAdditionalData();
         Log.d("OneSignalExample", "notification: " + notification.toJSONObject());
@@ -27,6 +30,10 @@ public class NotificationServiceExtension implements OSRemoteNotificationReceive
 
         // If complete isn't call within a time period of 25 seconds, OneSignal internal logic will show the original notification
         // To omit displaying a notification, pass `null` to complete()
-        notificationReceivedEvent.complete(mutableNotification);
+        notificationReceivedEvent.complete(notification);
+
+
+        ShortcutBadger.applyCount(context,mPrefs.getInt("BadgeCount", 0) + 1);
+        mPrefs.edit().putInt("BadgeCount",mPrefs.getInt("BadgeCount", 0) + 1).apply();
     }
 }
