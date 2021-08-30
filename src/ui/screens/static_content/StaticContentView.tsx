4cedfa70355f12c0b72aac82ef3af02486128684
Muhammad Noman <muhammad.nauman@startrum.com>
2021-08-30 17:43:26 +0500

    handle html content

--- src/ui/screens/static_content/StaticContentView.tsx
+++ src/ui/screens/static_content/StaticContentView.tsx
@@ -3,42 +3,74 @@ import { StyleSheet } from "react-native";
 import Screen from "ui/components/atoms/Screen";
 import { AppLog, shadowStyleProps } from "utils/Util";
 import { StaticContent } from "models/api_responses/StaticContentResponseModel";
-import { SPACE, STRINGS } from "config";
-import WebView from "react-native-webview";
+import { SPACE } from "config";
+import HtmlView from "react-native-htmlview";
+import { usePreferredTheme } from "hooks";
+import { ColorPalette } from "hooks/theme/ColorPaletteContainer";
 
 type Props = {
   staticContent: StaticContent;
 };
 
 export const StaticContentView = React.memo<Props>(({ staticContent }) => {
-  AppLog.log(() => "rendering StaticContentView");
+  AppLog.log(
+    () =>
+      "rendering StaticContentView : " +
+      JSON.stringify(String(staticContent.content))
+  );
+
+  const { themedColors } = usePreferredTheme();
   return (
-    <Screen style={styles.container} shouldAddBottomInset={false}>
-      <WebView
-        originWhitelist={["*"]}
-        source={{
-          html: `<html lang="en"><head><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>${
-            staticContent.title
-          }</title></head><body style="margin: 16px">${
-            staticContent.content ?? STRINGS.common.not_found
-          }</body></html>`
-        }}
-        containerStyle={styles.content}
+    <Screen
+      style={styles(themedColors).container}
+      shouldAddBottomInset={false}>
+      <HtmlView
+        addLineBreaks={true}
+        value={String(staticContent.content).replace("</li>", "</li>")}
+        stylesheet={styles(themedColors)}
       />
     </Screen>
   );
 });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  card: {
-    flex: 1,
-    borderRadius: 12,
-    margin: SPACE.lg,
-    overflow: "hidden",
-    ...shadowStyleProps
-  },
-  content: {}
-});
+const styles = (color: ColorPalette) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: SPACE.lg
+    },
+    card: {
+      flex: 1,
+      borderRadius: 12,
+      margin: SPACE.lg,
+      overflow: "hidden",
+      ...shadowStyleProps
+    },
+    content: {},
+    ul: {
+      fontSize: 18,
+      paddingBottom: 10
+    },
+    ol: {
+      fontSize: 18,
+      paddingBottom: 0,
+      marginBottom: 10
+    },
+    p: {
+      fontSize: 18,
+      marginBottom: -20,
+      paddingBottom: 0,
+      marginTop: 0
+    },
+    li: {
+      fontSize: 18,
+      paddingBottom: 0,
+      marginBottom: 10
+    },
+    a: {
+      color: color.primary
+    },
+    h4: {
+      fontSize: 22
+    }
+  });
